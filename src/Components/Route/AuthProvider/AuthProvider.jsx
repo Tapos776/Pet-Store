df0fd75fc01eadc30019004b3a3b42e24e7d42e4
tapos-chandro <taposrj85@gmail.com>
2025-09-10 23:55:23 +0600

    UserLogin Firebase setUp

--- src/Components/Route/AuthProvider/AuthProvider.jsx
+++ src/Components/Route/AuthProvider/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import { createContext } from "react";
+import { createUserWithEmailAndPassword, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
 import auth from "../../../firebase.config";
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -7,17 +7,38 @@ export  const AuthContext =  createContext(null)
 
 const provider = new GoogleAuthProvider();
 const AuthProvider = ({children}) => {
+    const [loading , setLoading]=useState(true);
+    const [user,setUser]=useState()
 
     const loginUser=(email,password)=>{
+        setLoading(true)
         return createUserWithEmailAndPassword(auth,email,password)
     }
     const googleLogin =()=>{
+        setLoading(true)
         return signInWithPopup(auth ,provider)
     }
+    const signInWithUser =(email,password)=>{
+        setLoading(true)
+        return signInWithEmailAndPassword (auth,email,password)
+    }
+
+    useEffect(()=>{
+      const unSubscribe=  onAuthStateChanged(auth, currentUser=>{
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return ()=>{
+            unSubscribe()
+        }
+    },[])
     
     const authInfo={
+        user,
+        loading,
         googleLogin,
         loginUser,
+        signInWithUser,
     }
     return (
         <AuthContext.Provider value={authInfo}>
