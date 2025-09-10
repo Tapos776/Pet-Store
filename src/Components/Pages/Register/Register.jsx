847a389fda2ce044710b578f3810ad188ae72d35
tapos-chandro <taposrj85@gmail.com>
2025-09-10 23:39:19 +0600

    firebase googleLogin Done

--- src/Components/Pages/Register/Register.jsx
+++ src/Components/Pages/Register/Register.jsx
@@ -1,9 +1,12 @@
 import Lottie from "lottie-react";
 import registerA from "../../../../public/pet/registerC.json"
 const Register = () => {
+    const userRegister =(e)=>{
+        e.preventDefault()
+    }
     return (
         <div className="hero bg-base-200  ">
-            <form >
+            <form onSubmit={userRegister} >
                 <h1 className=" mt-8 text-7xl font-bold bg-gradient-to-r from-green-500 via-green-900 to-green-900 bg-clip-text text-transparent  text-shadow-accent-content [text-shadow:0_5px_15px_rgb(255_0_0_/_0.8),0_-5px_15px_rgb(0_255_0_/_0.8)] text-center "> Register now!</h1>
                 <div className="hero-content flex-col lg:flex-row-reverse ">
                     <div className="text-center lg:text-left w-[30%]">
